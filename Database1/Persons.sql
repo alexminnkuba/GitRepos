3a35b308d81e78b34618769dc6ac76720631c9c1
alexminnkuba <aleksei.minn2014@gmail.com>
2025-03-30 01:34:13 +0400

    #TDP_1

---
+++ Database1/Persons.sql
@@ -0,0 +1,54 @@
+﻿CREATE TABLE [dbo].[Persons]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Surname] NVARCHAR(50) NOT NULL, 
+    [Name] NVARCHAR(50) NOT NULL, 
+    [Patronymic] NVARCHAR(50) NULL, 
+    [DateOfBirth] DATE NULL
+)
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Идентификатор лица',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Persons',
+    @level2type = N'COLUMN',
+    @level2name = N'Id'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Фамилия',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Persons',
+    @level2type = N'COLUMN',
+    @level2name = N'Surname'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Имя',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Persons',
+    @level2type = N'COLUMN',
+    @level2name = N'Name'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Отчество',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Persons',
+    @level2type = N'COLUMN',
+    @level2name = N'Patronymic'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Дата рождения',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Persons',
+    @level2type = N'COLUMN',
+    @level2name = N'DateOfBirth'
---
+++ Database1/sp_Persons_Select.sql
@@ -0,0 +1,2 @@
+﻿CREATE VIEW [dbo].[sp_Persons_Select]
+AS SELECT * FROM [Persons]
